0d2e3d90d15c9316315214e6d4a4b7464f9fcf4f
EddiesTech <eddiecoldrick@hotmail.co.uk>
2020-12-20 13:22:03 +0000

    Update client.js

--- public/client.js
+++ public/client.js
@@ -1,7 +1,7 @@
 function getWords() {
   ["predicates","objects","word-pairs","team-pairs","teams","collection-pairs","collections"].forEach((category) => {
     $.get("/.netlify/functions/words/"+category).then((data) => {
-      $("#"+category).text(data.join("\n"));
+      $("#"+category).text(data);
     });
   });
 }
